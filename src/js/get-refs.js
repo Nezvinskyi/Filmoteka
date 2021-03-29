53defbffe7e744025121301cb92f586edc71f4d8
RinaOcean <antyfifa@gmail.com>
2021-03-29 22:23:29 +0300

    решает проблему доступности рефов

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -1,7 +1,11 @@
 export default function getRefs() {
   return {
     bodyRef: document.querySelector('body'),
-    // searchForm: document.getElementById('search-form'),
-    // gallery: document.querySelector('.gallery'),
+    header: document.querySelector('.header'),
+    pagesNav: document.querySelector('.header-nav'),
+    homeNav: document.querySelector('.home-page-js'),
+    libNav: document.querySelector('.library-page-js'),
+    searchForm: document.getElementById('search-form'),
+    headerBtnWrapper: document.querySelector('.wrapper-btn-header'),
   };
 }
