c233c146d4811a6512fa7a4383a9e39fc85bb46a
RinaOcean <antyfifa@gmail.com>
2021-03-29 16:41:28 +0300

    добавляет переключение картинки бэкграунда по нажатию

--- src/js/header.js
+++ src/js/header.js
@@ -12,19 +12,27 @@ function addHeader() {
 addHeader();
 
 const searchFormRef = document.getElementById('search-form');
+const homeNavRef = document.querySelector('.home-page-js');
+const libNavRef = document.querySelector('.library-page-js');
 const pagesNav = document.querySelector('.header-nav');
+const headerRef = document.querySelector('.header');
 
 pagesNav.addEventListener('click', onNavClick);
 searchFormRef.addEventListener('submit', onSearch);
 
 function onNavClick(event) {
-  const home = event.target.dataset.home;
-  const library = event.target.dataset.library;
-  if (home) {
-    console.log('home');
+  if (event.target.dataset.action === 'home') {
+    libNavRef.classList.remove('current-page');
+    homeNavRef.classList.add('current-page');
+
+    headerRef.classList.remove('library-header');
   }
-  if (library) {
-    console.log('library');
+
+  if (event.target.dataset.action === 'library') {
+    homeNavRef.classList.remove('current-page');
+    libNavRef.classList.add('current-page');
+
+    headerRef.classList.add('library-header');
   }
 }
 
