f88acdf90e345329f27024bf682f3d106c2d38f5
Vitalii Susulovskyi <vitalinos1978@gmail.com>
2021-03-30 13:44:17 +0300

    добавляет стили пагинации + нерабочий фавикон + исход. состояние package.json

--- src/js/components/paginations.js
+++ src/js/components/paginations.js
@@ -1,19 +1,19 @@
-$('#demo').pagination({
-    dataSource: {BASE_URL},
-    locator: 'items',
-    totalNumberLocator: function(response) {
-        // you can return totalNumber by analyzing response content
-        return Math.floor(Math.random() * (1000 - 100)) + 100;
-    },
-    pageSize: 20,
-    ajax: {
-        beforeSend: function() {
-            dataContainer.html('Loading data from {BASE_URL} ...');
-        }
-    },
-    callback: function(data, pagination) {
-        // template method of yourself
-        var html = template(data);
-        dataContainer.html(html);
-    }
-})
+// $('#demo').pagination({
+//     dataSource: {BASE_URL},
+//     locator: 'items',
+//     totalNumberLocator: function(response) {
+//         // you can return totalNumber by analyzing response content
+//         return Math.floor(Math.random() * (1000 - 100)) + 100;
+//     },
+//     pageSize: 20,
+//     ajax: {
+//         beforeSend: function() {
+//             dataContainer.html('Loading data from {BASE_URL} ...');
+//         }
+//     },
+//     callback: function(data, pagination) {
+//         // template method of yourself
+//         var html = template(data);
+//         dataContainer.html(html);
+//     }
+// })
