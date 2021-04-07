76339d14d9fb611a68f2b3e8942bd55dda3021bb
iaroslav-sk <iaroslav.dev.sk@gmail.com>
2021-04-07 12:35:58 +0300

    changet color modal btn
    1. Меняет цвет кнопок и текст при добавлении/удалениии из библиотеки.
    2. Перебирает localStorage и  присваеват цвет и текст кнопкам при наличии или отсутвствии в   библиотеке.

--- src/js/api/storage.js
+++ src/js/api/storage.js
@@ -1,27 +1,37 @@
 import { onError, onFetchError, onInfo } from '../components/notifications';
+import getRefs from '../get-refs';
+const refs = getRefs();
+
+checkLocalStorage();
 
 export function onClickToWatchedHandler(movie) {
   const btnAddToWatchedRef = document.querySelector('.btn-js-addtowatched');
   btnAddToWatchedRef.addEventListener('click', clickBtn);
-  function clickBtn(event) {
-    if (localStorage.getItem('watched') === null) {
-      localStorage.setItem('watched', '[]');
-    }
+  function clickBtn() {
     saveToWatched(movie);
   }
+  actualyLibraryWached(movie);
 }
 
 export function onClickToQueueHandler(movie) {
   const btnAddToQueueRef = document.querySelector('.btn-js-addtoqueue');
   btnAddToQueueRef.addEventListener('click', clickBtn);
-  function clickBtn(event) {
-    if (localStorage.getItem('queue') === null) {
-      localStorage.setItem('queue', '[]');
-    }
+  function clickBtn() {
     saveToQueue(movie);
+  }
+  actualyLibraryQueue(movie);
+}
+function checkLocalStorage() {
+  if (localStorage.getItem('watched') === null) {
+    localStorage.setItem('watched', '[]');
+  }
+
+  if (localStorage.getItem('queue') === null) {
+    localStorage.setItem('queue', '[]');
   }
 }
 
+// Wached ====================================================
 function saveToWatched(movie) {
   let storage = JSON.parse(localStorage.getItem('watched'));
   const {
@@ -48,15 +58,46 @@ function saveToWatched(movie) {
   if (index > -1) {
     storage.splice(index, 1);
     onInfo('Deleted from watched');
+    deleteClassBtnWached();
   } else {
     storage.push(string);
     onInfo('Added to watched');
+    addClassBtnWached();
   }
   const stringFromObj = JSON.stringify(storage);
   let endStorage = JSON.parse(stringFromObj);
   localStorage.setItem('watched', stringFromObj);
   return endStorage;
 }
+
+function addClassBtnWached() {
+  const modalBtnWached = document.querySelector('.btn-js-addtowatched');
+  modalBtnWached.classList.remove('btn-active-page');
+  modalBtnWached.innerHTML = 'Delete from wached';
+}
+
+function deleteClassBtnWached() {
+  const modalBtnWached = document.querySelector('.btn-js-addtowatched');
+  modalBtnWached.classList.add('btn-active-page');
+  modalBtnWached.innerHTML = 'Add to wached';
+}
+
+function actualyLibraryWached(movie) {
+  const modalBtnWached = document.querySelector('.btn-js-addtowatched');
+  modalBtnWached.classList.add('btn-active-page');
+  let storageSaved = JSON.parse(localStorage.getItem('watched'));
+  storageSaved.forEach(element => {
+    const elementOfStorage = JSON.parse(element);
+    const findedMovie = elementOfStorage.id === movie.id;
+    if (findedMovie) {
+      const modalBtnWached = document.querySelector('.btn-js-addtowatched');
+      modalBtnWached.classList.remove('btn-active-page');
+      modalBtnWached.innerHTML = 'Delete from wached';
+      return;
+    }
+  });
+}
+// Queue ====================================================
 
 function saveToQueue(movie) {
   let storage = JSON.parse(localStorage.getItem('queue'));
@@ -84,12 +125,42 @@ function saveToQueue(movie) {
   if (index > -1) {
     storage.splice(index, 1);
     onInfo('Deleted from queue');
+    addClassBtnQueue();
   } else {
     storage.push(string);
     onInfo('Added to queue');
+    deleteClassBtnQueue();
   }
   const stringFromObj = JSON.stringify(storage);
   let endStorage = JSON.parse(stringFromObj);
   localStorage.setItem('queue', stringFromObj);
   return endStorage;
+}
+
+function addClassBtnQueue() {
+  const modalBtn = document.querySelector('.btn-js-addtoqueue');
+  modalBtn.classList.add('btn-active-page');
+  modalBtn.innerHTML = 'Add to queue';
+}
+
+function deleteClassBtnQueue() {
+  const modalBtnQueue = document.querySelector('.btn-js-addtoqueue');
+  modalBtnQueue.classList.remove('btn-active-page');
+  modalBtnQueue.innerHTML = 'Delete from queue';
+}
+
+function actualyLibraryQueue(movie) {
+  const modalBtnQueue = document.querySelector('.btn-js-addtoqueue');
+  modalBtnQueue.classList.add('btn-active-page');
+  let storageSaved = JSON.parse(localStorage.getItem('queue'));
+  storageSaved.forEach(element => {
+    const elementOfStorage = JSON.parse(element);
+    const findedMovie = elementOfStorage.id === movie.id;
+    if (findedMovie) {
+      const modalBtnQueue = document.querySelector('.btn-js-addtoqueue');
+      modalBtnQueue.classList.remove('btn-active-page');
+      modalBtnQueue.innerHTML = 'Delete from queue';
+      return;
+    }
+  });
 }
