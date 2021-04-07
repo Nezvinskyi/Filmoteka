84699137e0e7407c7918d9f01f0271f76255165c
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-04-08 00:11:56 +0200

    BugFix вход в библиотеку

--- src/js/api/db.js
+++ src/js/api/db.js
@@ -6,7 +6,9 @@ const { DB_URL } = settings;
 import film from '../film.json';
 
 class DbInterface {
-  constructor() {}
+  constructor() {
+    this.watched = '';
+  }
 
   addToWatched(data) {
     const url = `${DB_URL}/users/${authUser.userId}/queue.json?auth=${authUser.token}`;
@@ -24,25 +26,42 @@ class DbInterface {
 
   addToQueue(data) {}
 
-  getAllWatchedData() {
+  async getAllWatchedData() {
     const url = `${DB_URL}/users/${authUser.userId}/watched.json?auth=${authUser.token}`;
-
-    fetch(url, {
+    let watched = null;
+    const params = {
       method: 'GET',
       headers: {
         'Content-type': 'application/json',
       },
-    })
-      .then(response => response.json())
-      .then(data => localStorage.setItem('watched-123', JSON.stringify(data)));
+    };
+    const allWatched = await this.fetch(url, params);
+
+    return allWatched;
+  }
+
+  async fetch(url, params) {
+    try {
+      const response = await fetch(url, params);
+      if (!response.ok) throw response.status;
+      return await response.json();
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+
+  getDbId(movieId) {
+    const watchedIds = Object.entries(data);
+    const targetId = watchedIds.find(([id, obj]) => obj.id === movieId)[0];
+    return targetId;
   }
 
   getFromStorage() {
     const parsedString = localStorage.getItem('watched-123');
     // console.log(JSON.parse(parsedString));
     // console.log(parsedString);
-    const a = Object.entries(parsedString);
-    console.log(a);
+    // const a = Object.entries(parsedString);
+    // console.log(a);
   }
 
   getDbId(data, movieId) {
@@ -54,7 +73,7 @@ class DbInterface {
 const dbUi = new DbInterface();
 setTimeout(() => {
   // dbUi.addToWatched(film);
-  console.log(dbUi.getAllWatchedData());
+  dbUi.getAllWatchedData();
   // console.log(authUser.token);
 }, 1000);
 
